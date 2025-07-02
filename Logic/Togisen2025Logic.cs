5e2855820663da5cd03410b6f783cca133371f76
AFEEjp <info@afee.jp>
2025-07-02 18:50:36 +0900

    ISS-5:2025年東京都都議会議員選挙対応 (#6)
    * 辞書
    
    * 2024年衆院選分のロジックを移動
    
    * 2025年都議選用ロジック

---
+++ Logic/Togisen2025Logic.cs
@@ -0,0 +1,168 @@
+﻿using System.Text;
+using System.Text.Json;
+using ElectionPageCreateTool.Extensions;
+using ElectionPageCreateTool.Model;
+
+namespace ElectionPageCreateTool.Logic;
+
+public static class Togisen2025Logic
+{
+    public static readonly DateTime NoticeDate = new DateTime(2025, 6, 13);
+
+    public static void Generate(string json, string directoryPath)
+    {
+        // JSONをクラスのリストにパース
+        var list = JsonSerializer.Deserialize<List<Togisen2025Respond>>(json);
+
+        if (list == null)
+        {
+            Console.WriteLine("パース失敗？");
+            return;
+        }
+
+
+        // カナの値を利用して並び替え
+        list = list.OrderBy(r => r.Namekana).ToList();
+
+        // 選挙区ごとに候補者情報を分ける
+        var removeRecords = new List<Togisen2025Respond>();
+        var 選挙区出力内容Dic = new Dictionary<Togisen2025Block, List<Togisen2025Respond>>();
+        foreach (var blk in Enum.GetValues<Togisen2025Block>())
+        {
+            var blockName = blk.GetDescription();
+
+            var respondList = new List<Togisen2025Respond>();
+            foreach (var togisen2025Respond in list.Where(r => r.Constituency == blockName))
+            {
+                // スプレッドシートの値が間違っているものについて調整
+                switch (togisen2025Respond.Name)
+                {
+                    case "原忠信":
+                        togisen2025Respond.Namekana = "はらただのぶ";
+                        break;
+                    case "須山　たかし":
+                        togisen2025Respond.PartyName = "国民民主党";
+                        break;
+                    case "鴇崎　直行":
+                        togisen2025Respond.Name = "ときざき　直行";
+                        break;
+                    case "○ すどう　健太郎":
+                        togisen2025Respond.Name = "すどう　健太郎";
+                        break;
+                    case "きとう直樹":
+                        togisen2025Respond.Birthdate = "1983/10/02";
+                        break;
+                    case "みぞぐち晃一":
+                    case "森　ひでひこ":
+                    case "梅木　大輝":
+                    case "太田　あつし":
+                        // 不出馬なのでリストから除外
+                        continue;
+                }
+
+                removeRecords.Add(togisen2025Respond);
+                respondList.Add(togisen2025Respond);
+            }
+
+            // 取得した分を元のリストから削除
+            foreach (var rec in removeRecords)
+            {
+                list.Remove(rec);
+            }
+
+            選挙区出力内容Dic.Add(blk, respondList);
+        }
+
+
+        var strBldr = new StringBuilder();
+
+        // 出力
+
+        foreach (var kv in 選挙区出力内容Dic)
+        {
+            strBldr.AppendLine($"<h3 id='{kv.Key.ToString()}'>{kv.Key.GetDescription()}</h3>");
+            strBldr.AppendLine("<div class='kohosya-info')><ol class='distList'>");
+
+            foreach (var resp in kv.Value)
+            {
+                strBldr.AppendLine("<li><ol><li><dl>");
+                strBldr.AppendLine($"<li class='kohosya-name'>{(resp.Result == "当選" ? "\ud83c\udf38 " : string.Empty)}{resp.Name.Replace("　", " ")}<span>（{resp.Namekana.Replace("　", " ")}）</span> : <span>{resp.PartyName}</span></li>");
+
+                if (Togisen2025Yakusoku.約束賛同者.Contains(resp.Namekana.Replace("　", "").Replace(" ", "")))
+                {
+                    strBldr.AppendLine($"<li class='yakusoku'><b>表現の自由を守るための約束 賛同者</b></li>");
+                }
+
+                // 誕生日
+                if (!string.IsNullOrEmpty(resp.Birthdate) && DateTime.TryParse(resp.Birthdate, out var birthdate))
+                {
+                    var age = NoticeDate.Year - birthdate.Year;
+                    if (birthdate > NoticeDate.AddYears(-age)) age--; // 今年の誕生日がまだ来ていない場合
+                    strBldr.AppendLine($"<li class='birthdate'><b>誕生日：</b><br>{birthdate:yyyy/MM/dd} （{age}歳）</li>");
+                }
+
+                var isNoResponse = resp.AdditionalInformation is "無回答" or "回答なし";
+
+                strBldr.AppendLine($"<li class='setsumon'><b>設問(1)：</b><br>{(isNoResponse ? "無回答" : resp.Q1Answer.ModifyQ1Answer())}</li>");
+                strBldr.AppendLine($"<li class='setsumon'><b>設問(1)の理由：</b><br>{(isNoResponse ? "無回答" : resp.Q1Reason)}</li>");
+                strBldr.AppendLine($"<li class='setsumon'><b>設問(2)：</b><br>{(isNoResponse ? "無回答" : resp.Q2Answer.ModifyQ2Answer())}</li>");
+                strBldr.AppendLine($"<li class='setsumon'><b>設問(3)：</b><br>{resp.FavoriteMangaAnimeGame}</li>");
+
+                if (!string.IsNullOrEmpty(resp.AdditionalInformation) && resp.AdditionalInformation != "無回答")
+                    strBldr.AppendLine($"<li class='setsumon'><b>補足：</b><br>{resp.AdditionalInformation.Replace("  ", "<br>")}</li>");
+
+                strBldr.AppendLine("</dl></li></ol></li>");
+
+
+                Console.WriteLine($"名前: {resp.Name}");
+                Console.WriteLine($"カナ: {resp.Namekana}");
+                Console.WriteLine($"誕生日: {resp.Birthdate}");
+                Console.WriteLine($"選挙区: {resp.Constituency}");
+                Console.WriteLine($"所属政党: {resp.PartyName}");
+                Console.WriteLine($"Q1: {resp.Q1Answer}");
+                Console.WriteLine($"Q1の理由: {resp.Q1Reason}");
+                Console.WriteLine($"Q2: {resp.Q2Answer}");
+                Console.WriteLine($"好きなマンガ・アニメ・ゲーム: {resp.FavoriteMangaAnimeGame}");
+                Console.WriteLine($"補足: {resp.AdditionalInformation}");
+                Console.WriteLine($"--------------------------------------------------------");
+                Console.WriteLine();
+            }
+
+            strBldr.AppendLine("</div>");
+        }
+
+        var 出力 = strBldr.ToString();
+
+        File.WriteAllText(Path.Combine(directoryPath, "senkyoku.html"), 出力);
+    }
+
+
+
+    public static string ModifyQ1Answer(this string q1Answer)
+    {
+        // 特定の回答を修正
+        return q1Answer switch
+        {
+            "安易な図書指定がなされないように条例と運用を見直すべきだ" => "A. 安易な図書指定がなされないように条例と運用を見直すべきだ",
+            "現状のままでよい・わからない" => "B. 現状のままでよい・わからない",
+            "さらに図書指定による規制を強化するべきだ" => "C. さらに図書指定による規制を強化するべきだ",
+            _ => q1Answer
+        };
+    }
+
+
+    public static string ModifyQ2Answer(this string q2Answer)
+    {
+        // 特定の回答を修正
+        return q2Answer switch
+        {
+            "民間の自主規制が十分機能している" => "A. 民間の自主規制が十分機能している",
+            "民間の自主規制がある程度機能している" => "B. 民間の自主規制がある程度機能している",
+            "どちらともいえない" => "C. どちらともいえない",
+            "法令による規制がある程度あった方がよい" => "D. 法令による規制がある程度あった方がよい",
+            "法令による厳格な規制が必要である" => "E. 法令による厳格な規制が必要である",
+            _ => q2Answer
+        };
+    }
+}
+
---
+++ Model/Togisen2025Block.cs
@@ -0,0 +1,176 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElectionPageCreateTool.Model
+{
+    public enum Togisen2025Block
+    {
+        [Display(Name = "千代田区")]
+        [Description("千代田区")]
+        Chiyodaku,
+
+        [Display(Name = "中央区")]
+        [Description("中央区")]
+        Chuoku,
+
+        [Display(Name = "港区")]
+        [Description("港区")]
+        Minatoku,
+
+        [Display(Name = "新宿区")]
+        [Description("新宿区")]
+        Shinjukuku,
+
+        [Display(Name = "文京区")]
+        [Description("文京区")]
+        Bunkyoku,
+
+        [Display(Name = "台東区")]
+        [Description("台東区")]
+        Taitoku,
+
+        [Display(Name = "墨田区")]
+        [Description("墨田区")]
+        Sumidaku,
+
+        [Display(Name = "江東区")]
+        [Description("江東区")]
+        Kotoku,
+
+        [Display(Name = "品川区")]
+        [Description("品川区")]
+        Shinagawaku,
+
+        [Display(Name = "目黒区")]
+        [Description("目黒区")]
+        Meguroku,
+
+        [Display(Name = "大田区")]
+        [Description("大田区")]
+        Otaku,
+
+        [Display(Name = "世田谷区")]
+        [Description("世田谷区")]
+        Setagayaku,
+
+        [Display(Name = "渋谷区")]
+        [Description("渋谷区")]
+        Shibuyaku,
+
+        [Display(Name = "中野区")]
+        [Description("中野区")]
+        Nakanoku,
+
+        [Display(Name = "杉並区")]
+        [Description("杉並区")]
+        Suginamiku,
+
+        [Display(Name = "豊島区")]
+        [Description("豊島区")]
+        Toshimaku,
+
+        [Display(Name = "北区")]
+        [Description("北区")]
+        Kitaku,
+
+        [Display(Name = "荒川区")]
+        [Description("荒川区")]
+        Arakawaku,
+
+        [Display(Name = "板橋区")]
+        [Description("板橋区")]
+        Itabashiku,
+
+        [Display(Name = "練馬区")]
+        [Description("練馬区")]
+        Nerimaku,
+
+        [Display(Name = "足立区")]
+        [Description("足立区")]
+        Adachiku,
+
+        [Display(Name = "葛飾区")]
+        [Description("葛飾区")]
+        Katsushikaku,
+
+        [Display(Name = "江戸川区")]
+        [Description("江戸川区")]
+        Edogawaku,
+
+        [Display(Name = "八王子市")]
+        [Description("八王子市")]
+        Hachioujishi,
+
+        [Display(Name = "立川市")]
+        [Description("立川市")]
+        Tachikawashi,
+
+        [Display(Name = "武蔵野市")]
+        [Description("武蔵野市")]
+        Musashinoshi,
+
+        [Display(Name = "三鷹市")]
+        [Description("三鷹市")]
+        Mitakashi,
+
+        [Display(Name = "青梅市")]
+        [Description("青梅市")]
+        Oumeshi,
+
+        [Display(Name = "府中市")]
+        [Description("府中市")]
+        Fuchuushi,
+
+        [Display(Name = "昭島市")]
+        [Description("昭島市")]
+        Akishimashi,
+
+        [Display(Name = "町田市")]
+        [Description("町田市")]
+        Machidashi,
+
+        [Display(Name = "小金井市")]
+        [Description("小金井市")]
+        Koganeishi,
+
+        [Display(Name = "小平市")]
+        [Description("小平市")]
+        Kodairashi,
+
+        [Display(Name = "日野市")]
+        [Description("日野市")]
+        Hinoshi,
+
+        [Display(Name = "西東京市")]
+        [Description("西東京市")]
+        NishiTokyoshi,
+
+        [Display(Name = "西多摩")]
+        [Description("西多摩")]
+        NishiTama,
+
+        [Display(Name = "南多摩")]
+        [Description("南多摩")]
+        MinamiTama,
+
+        [Display(Name = "北多摩第一")]
+        [Description("北多摩第一")]
+        KitaTamaDaiichi,
+
+        [Display(Name = "北多摩第二")]
+        [Description("北多摩第二")]
+        KitaTamaDaini,
+
+        [Display(Name = "北多摩第三")]
+        [Description("北多摩第三")]
+        KitaTamaDaisan,
+
+        [Display(Name = "北多摩第四")]
+        [Description("北多摩第四")]
+        KitaTamaDaiyon,
+
+        [Display(Name = "島部")]
+        [Description("島部")]
+        Tobu
+    }
+}
